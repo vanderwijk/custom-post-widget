d26a8db37907d6157350ebce2806c185232c4ba2
Johan van der Wijk <johan@vanderwijk.nl>
2024-09-03 15:36:57 +0200

    Update content_block messages for custom post widget

--- post-type.php
+++ post-type.php
@@ -37,15 +37,22 @@ add_action( 'init', 'cpw_post_type_init' );
 function content_block_messages( $messages ) {
 	$messages['content_block'] = array(
 		0 => '',
+		// translators: Placeholder is the URL for managing widgets.
 		1 => current_user_can( 'edit_theme_options' ) ? sprintf( __( 'Content Block updated. <a href="%s">Manage Widgets</a>', 'custom-post-widget' ), esc_url( 'widgets.php' ) ) : sprintf( __( 'Content Block updated.', 'custom-post-widget' ), esc_url( 'widgets.php' ) ),
+		// translators: Placeholder is the custom field.
 		2 => __( 'Custom field updated.', 'custom-post-widget' ),
 		3 => __( 'Custom field deleted.', 'custom-post-widget' ),
 		4 => __( 'Content Block updated.', 'custom-post-widget' ),
+		// translators: Placeholder is the revision title.
 		5 => isset($_GET['revision']) ? sprintf( __( 'Content Block restored to revision from %s', 'custom-post-widget' ), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+		// translators: Placeholder is the URL for managing widgets.
 		6 => current_user_can( 'edit_theme_options' ) ? sprintf( __( 'Content Block published. <a href="%s">Manage Widgets</a>', 'custom-post-widget' ), esc_url( 'widgets.php' ) ) : sprintf( __( 'Content Block published.', 'custom-post-widget' ), esc_url( 'widgets.php' ) ),
 		7 => __( 'Block saved.', 'custom-post-widget' ),
+		// translators: Placeholder is the URL for managing widgets.
 		8 => current_user_can( 'edit_theme_options' ) ? sprintf( __( 'Content Block submitted. <a href="%s">Manage Widgets</a>', 'custom-post-widget' ), esc_url( 'widgets.php' ) ) : sprintf( __( 'Content Block submitted.', 'custom-post-widget' ), esc_url( 'widgets.php' ) ),
+		// translators: Placeholder is the scheduled date and time.
 		9 => sprintf( __( 'Content Block scheduled for: <strong>%1$s</strong>.', 'custom-post-widget' ), date_i18n( __( 'M j, Y @ G:i' , 'custom-post-widget' ), strtotime(isset($post->post_date) ? $post->post_date : '') ), esc_url( 'widgets.php' ) ),
+		// translators: Placeholder is the URL for managing widgets.
 		10 => current_user_can( 'edit_theme_options' ) ? sprintf( __( 'Content Block draft updated. <a href="%s">Manage Widgets</a>', 'custom-post-widget' ), esc_url( 'widgets.php' ) ) : sprintf( __( 'Content Block draft updated.', 'custom-post-widget' ), esc_url( 'widgets.php' ) ),
 	);
 	return $messages;
